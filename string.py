4b50880b7a3073e615fd2405744096dc27e048f6
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-02-05 02:36:05 +0700

    string part1

---
+++ string.py
@@ -0,0 +1,18 @@
+
+# Nháy đơn
+strName = 'Tuong'
+print(strName)
+
+# Nháy kép
+strFullName = "Dinh Manh Tuong's"
+print(strFullName)
+
+
+stingDemo = "\'I\'m \"Tataro\""
+stingDemo1 = """ 'I'm "Tataro" """
+stingDemo2 = ''' 'I'm "Tataro" '''
+stingDemo3 = "\'I\\m \"Tataro\""
+print(stingDemo)
+print(stingDemo1)
+print(stingDemo2)
+print(stingDemo3)
